c480c530d0480dabcc673d8d9146ae89e10c55e1
Maria Viloria <mviloria134@gmail.com>
2023-11-10 02:10:05 +0000

    first version Python program

---
+++ mviloria11.py
@@ -0,0 +1,8 @@
+#Maria Viloria
+#CSC 321
+#Lab 11
+
+num = float(input("Enter a number: "))
+saying = "the early bird gets the worm, but the second mouse gets the cheese"
+print(f"{num} squared is {num * num}")
+print(f"And remember...{saying}!")
